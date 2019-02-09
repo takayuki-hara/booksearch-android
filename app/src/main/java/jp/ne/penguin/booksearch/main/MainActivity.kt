c03cc27190f6c11cedbee55d540b84cdc82d4a37
takayuki-hara <takayuki.hara.dev@gmail.com>
2019-02-09 12:07:23 +0900

    MainActivityを移動

--- app/src/main/java/jp/ne/penguin/booksearch/main/MainActivity.kt
+++ app/src/main/java/jp/ne/penguin/booksearch/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package jp.ne.penguin.booksearch
+package jp.ne.penguin.booksearch.main
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
@@ -8,6 +8,7 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import jp.ne.penguin.booksearch.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
