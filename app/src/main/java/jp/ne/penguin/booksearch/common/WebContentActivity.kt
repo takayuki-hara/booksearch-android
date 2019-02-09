c7faae41844de282ae4592d4ef12f92003d7c1f5
takayuki-hara <takayuki.hara.dev@gmail.com>
2019-02-09 17:01:25 +0900

    WebContentActivityに戻るボタンを追加

--- app/src/main/java/jp/ne/penguin/booksearch/common/WebContentActivity.kt
+++ app/src/main/java/jp/ne/penguin/booksearch/common/WebContentActivity.kt
@@ -2,6 +2,7 @@ package jp.ne.penguin.booksearch.common
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.webkit.WebViewClient
 import jp.ne.penguin.booksearch.R
 import kotlinx.android.synthetic.main.activity_web_content.*
@@ -14,5 +15,18 @@ class WebContentActivity : AppCompatActivity() {
 
         webView.webViewClient = WebViewClient()
         webView.loadUrl("https://www.google.co.jp/")
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
