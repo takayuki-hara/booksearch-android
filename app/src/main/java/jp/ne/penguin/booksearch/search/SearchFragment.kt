dd5cac6fd4bda494330468ae8e2540479233a934
takayuki-hara <takayuki.hara.dev@gmail.com>
2019-02-09 15:01:32 +0900

    検索条件、検索履歴画面、Adapterの追加

--- app/src/main/java/jp/ne/penguin/booksearch/search/SearchFragment.kt
+++ app/src/main/java/jp/ne/penguin/booksearch/search/SearchFragment.kt
@@ -1,14 +1,14 @@
 package jp.ne.penguin.booksearch.search
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import jp.ne.penguin.booksearch.R
+import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
     companion object {
@@ -26,5 +26,18 @@ class SearchFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        val fragmentManager : FragmentManager = childFragmentManager
+        val adapter = SearchFragmentPagerAdapter(fragmentManager)
+        viewPager.adapter = adapter
+        viewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+            override fun onPageScrolled(pos: Int, posOffset: Float, posOffsetPixels: Int) {}
+            override fun onPageSelected(pos: Int) {
+                adapter.refresh()
+            }
+
+        })
+        tabLayout.setupWithViewPager(viewPager)
     }
 }
---
+++ app/src/main/java/jp/ne/penguin/booksearch/search/SearchHistoryFragment.kt
@@ -0,0 +1,20 @@
+package jp.ne.penguin.booksearch.search
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import jp.ne.penguin.booksearch.R
+
+class SearchHistoryFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_search_history, container, false)
+    }
+
+
+}
