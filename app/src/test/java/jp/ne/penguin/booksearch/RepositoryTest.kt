1d3cee91b2cde64cdf629b49ccf2dfbbb0396b1b
takayuki-hara <takayuki.hara.dev@gmail.com>
2019-05-02 13:16:56 +0900

    リポジトリをテストするコードを追加

---
+++ app/src/test/java/jp/ne/penguin/booksearch/RepositoryTest.kt
@@ -0,0 +1,39 @@
+package jp.ne.penguin.booksearch
+
+import io.reactivex.schedulers.Schedulers
+import jp.ne.penguin.booksearch.data.repository.BookSearchRepository
+import org.junit.Assert
+import org.junit.Test
+
+class RepositoryTest {
+
+    fun sample() {
+        // メインのコードで呼ぶ際のサンプル
+        //BookSearchRepository().search("android", 1, "001005")
+        //        .subscribeOn(Schedulers.io())
+        //        .subscribeBy(
+        //                onSuccess = {
+        //                    Log.d("LOG", "Success")
+        //                },
+        //                onError = {
+        //                    Log.d("LOG", "Error")
+        //                }
+        //        )
+    }
+
+    @Test
+    fun test_getUsers() {
+        val result = BookSearchRepository().getUsers()
+                .subscribeOn(Schedulers.io())
+                .test()
+                .await()
+                .assertNoErrors()
+                .assertComplete()
+                .values().get(0)
+        println(result)
+
+        // ココは必要に応じて調整する
+        Assert.assertEquals(result.get(0).id, 2)
+    }
+
+}
