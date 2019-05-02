80a09d14438e493eccec618e17ded73aec75efcd
takayuki-hara <takayuki.hara.dev@gmail.com>
2019-05-02 14:15:08 +0900

    リファクタリング

--- app/src/main/java/jp/ne/penguin/booksearch/data/entity/User.kt
+++ app/src/main/java/jp/ne/penguin/booksearch/data/entity/User.kt
@@ -1,17 +1,24 @@
 package jp.ne.penguin.booksearch.data.entity
 
+import com.google.gson.annotations.SerializedName
 import java.util.*
 
 data class User (
     var id: Int,
     var username: String,
-    var password_digest: String,
+    @SerializedName("password_digest")
+    var passwordDigest: String,
     var token: String,
     var role: Int,
     var email: String,
-    var fcm_token: String,
-    var enable_email_notify: Boolean,
-    var enable_fcm_notify: Boolean,
-    var created_at: Date,
-    var updated_at: Date
+    @SerializedName("fcm_token")
+    var fcmToken: String,
+    @SerializedName("enable_email_notify")
+    var enableEmailNotify: Boolean,
+    @SerializedName("enable_fcm_notify")
+    var enableFcmNotify: Boolean,
+    @SerializedName("created_at")
+    var createdAt: Date,
+    @SerializedName("updated_at")
+    var updatedAt: Date
 )
